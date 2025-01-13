3facd4d8d315e90e4dc4b25b88ac4b35b3f7b5a0
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-01-13 13:55:59 +0100

    Initial commit

---
+++ test/saleor-app-aws-lambda-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SaleorAppAwsLambdaTemplate from '../lib/saleor-app-aws-lambda-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/saleor-app-aws-lambda-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SaleorAppAwsLambdaTemplate.SaleorAppAwsLambdaTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
